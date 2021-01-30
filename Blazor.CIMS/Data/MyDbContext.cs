22be4e825eb99daa7dadd3634f1d5d55bf99fb8a
Mao Sochan <sochan.mao@gmail.com>
2021-01-30 13:43:31 +1100

    ss

--- Blazor.CIMS/Data/MyDbContext.cs
+++ Blazor.CIMS/Data/MyDbContext.cs
@@ -7,6 +7,7 @@ namespace Blazor.CIMS.Data
         public MyDbContext(DbContextOptions<MyDbContext> options)
             : base(options)
         {
+
         }
         public DbSet<Computer> Computers { get; set; }
         public DbSet<Tablet> Tablets { get; set; }
