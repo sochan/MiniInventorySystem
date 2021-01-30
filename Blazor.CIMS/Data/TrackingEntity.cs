aed0993a5cb4bc3034b7adc73dd10b67e2f4c6d6
Mao Sochan <sochan.mao@gmail.com>
2021-01-30 12:15:47 +1100

    Why version control did not taking care this

---
+++ Blazor.CIMS/Data/TrackingEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Blazor.CIMS.Data
+{
+    public class TrackingEntity
+    {
+        public bool? IsActive { get; set; }
+        public bool? IsDeleted { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        
+    }
+}
